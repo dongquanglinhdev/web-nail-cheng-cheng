2d5d1fa6342fdf132d6cbd8b90f6267934645d27
dong.linh (Gosei) <dong.quang.linh@gosei.com.vn>
2026-01-30 18:24:19 +0700

    Fix: Update config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/nail-web/',
+  base: '/',
 })
